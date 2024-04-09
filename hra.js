43efd0dfe7ca791b3340d8567ae24811ebe0e22d
TomasHoncik <hon42367@spseol.cz>
2024-04-09 02:09:31 +0200

    first commit

---
+++ hra.js
@@ -0,0 +1,57 @@
+
+alert("Pro začátek si otevři konzoli")
+
+
+console.log("Vítej, dobrodruhu, na planetě X, domově poklidné civilizace Xenonů.")
+console.log("Slyšel jsem o tvém příchodu, kterým se naděje obrací k nám všem. Jsem Hadrian, strážce tajemství a moudrosti této země a mám pro tebe úkol.")
+
+console.log("Temné nebezpečí hrozí této krásné planetě. Zlověstný Y (Energico), energetický parazit, pronikl do našeho světa a hrozí zničením veškerého života.")
+console.log("Tvůj úkol je jednoznačný, hrdino. Posbírej brnění, naostři si meč a zachraň náš svět.")
+alert("Jsi připraven?")
+
+console.log("Ještě počkej, mám pro tebe dárek.") 
+console.log("*vytáhne meč*")
+console.log("Tento meč byl uložen v bezpečí mého chrámu, čekajíc na hrdinu, který přinese světlo do naší temné doby.") 
+console.log("Bohužel jsem již starý a nemám sílu, proto jsem ti ho nemohl naostřit. To tedy bude tvým prvním úkolem") 
+
+console.log("Naostři si meč ale pozor pokud meč naostříš málo nebo  až příliš bude ti k ničemu.")
+
+while (true) {
+    let mec = prompt("podržením libovolné klávesy dostatečně dlouho naostříš meč: ")
+    if (mec.length <= 10){
+        console.log("Naostřil jsi meč příliš málo, zkus to znovu")
+    }
+    else if(mec.length >= 20){
+        console.log("Naostřil jsi meč až příliš, zkus to znovu")
+    }
+    else{
+        console.log("Meč jsi naostřil skvěle")
+        break
+    }
+}
+
+
+
+console.log("Výtečně, vidím, že naše naděje nebyly mylné. Tvůj meč je ostřejší než kdy dřív, hrdino. S takovým nástrojem na své straně, budeš neporazitelný.")
+console.log("Ale přeci jenom, chtělo by to ještě brnění… Nedaleko odsud je stará kovárna zkus se v ní porozhlédnout jestli něco neobjevíš")
+while (true){
+
+
+    console.log("[1 - Podívat se do staré kovárny]")
+    console.log("[2 - utéct a už se nikdy nevrátit]")
+    let volba = prompt("vyber si: ")
+    if(volba == 1){
+
+    Skvěle, jde vidět, že máš pro strach uděláno.
+    Vcházíš do 
+
+
+    }elif(volba == 2){
+    Rozhodl si se, že práce hrdiny celé planety není nic pro tebe a rozběhl si se. Bohužel si cestou pryč zakopl o kámen a nabodnul se na něj.
+    R.I.P
+
+
+    }else{
+    Něco se nepovedlo, zkus to znovu
+    continue} */
+}
